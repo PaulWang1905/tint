043830668cc9ecb83f322fb67c2a73fccd39e1d3
Puyu Wang <shavingbowl@hotmail.com>
2018-01-29 16:08:57 +0000

    add the big O notation

---
+++ stage2/big_O_notation.py
@@ -0,0 +1,32 @@
+n = 1000
+a = list(range(n))
+b = dict.fromkeys(range(n))
+for i in range(1000):
+    i in a
+    i in b
+def o_one(items):
+    return 1 # 1 operation so O(1)
+def o_n(items):
+    total = 0
+    # Walks through all items once so O(n)
+    for item in items:
+        total += item
+    return total
+
+def o_n_squared(items):
+    total = 0 
+    # Walks through all items n*n times so O(n**2)
+    for a in items:
+        for b in items:
+            total += a * b
+
+    return total
+
+n = 10
+items = range(n)
+k = o_one(items)
+i = o_n(items)
+j = o_n_squared(items)
+print(k)
+print(i)
+print(j)
