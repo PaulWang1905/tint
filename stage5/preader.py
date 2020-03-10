7a76cef1c4e065bbd95966b22ddef46671920001
Paulwang <puyuwang@live.com>
2020-03-10 16:44:57 +0000

    adding pitch reader function

---
+++ stage5/preader.py
@@ -0,0 +1,14 @@
+
+def read_data(read_input_note_data):
+    # read the input data, and return a list. 
+    f = open(read_input_note_data)
+    note_dict=[]
+    for line in f:
+        note = line.split(',')[0]
+        note = str(int(note)%12)
+        duration = line.strip().split(',')[1]
+        note_dict.append((note,duration))
+    return note_dict 
+
+a = read_data('input.txt')
+print(a)
