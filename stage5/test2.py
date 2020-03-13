e878e8ee3b718c2f51090ec209c2fa21b5919548
Paulwang <puyuwang@live.com>
2020-03-13 17:18:07 +0000

    update a class

---
+++ stage5/test2.py
@@ -0,0 +1,19 @@
+import pitchcalculator
+
+class InputNote:
+    def __init__(self, pitch, duration):
+        
+        self.pitch = pitch
+        #print("getting pitch")
+        self.duration = duration
+        # cross out non diatonic notes temporarliy 
+        # But the chromatic notes need to be processed later. 
+        if pitch in (1, 3, 6, 8, 10):
+            print("not in scale")
+        else:
+            #print("getting degree by calling the function")
+            self.degree = pitchcalculator.pitch_to_degree(pitch)
+
+
+
+
